3a986ad39a418f0930c51281920a6960e1b99512
AkinpeluOluwatayo <akinpeluoluwatayo1235@gmail.com>
2026-02-06 13:30:29 +0100

    done

--- src/main/java/enterprise/elroi/controller/AuthController.java
+++ src/main/java/enterprise/elroi/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://zenith-expense-tracker.vercel.app/")
 @RequestMapping("/zenith/auth")
 public class AuthController {
 
--- src/main/java/enterprise/elroi/controller/TransactionController.java
+++ src/main/java/enterprise/elroi/controller/TransactionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://zenith-expense-tracker.vercel.app/")
 @RequestMapping("/zenith/transactions")
 public class TransactionController {
 
